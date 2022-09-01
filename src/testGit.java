5fe9c1b09012dc5f5548a74f14c0963627fe621b
IRUoi <pretvorniki@gmail.com>
2022-09-01 11:17:31 +0800

    testGit

---
+++ src/testGit.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+/**
+ * @Auther: ZHU(lc))
+ * @Date: 9/1/2022-09-01-11:16 AM
+ * @Description：PACKAGE_NAME
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("testGit");
+    }
+}
