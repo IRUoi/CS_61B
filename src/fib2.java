7b18a06d4626d0d5c9aa5fb630d6fd457a13178c
IRUoi <pretvorniki@gmail.com>
2022-09-01 11:12:53 +0800

    test2

--- src/fib2.java
+++ src/fib2.java
@@ -1,9 +1,15 @@
-package PACKAGE_NAME;
-
 /**
  * @Auther: ZHU(lc))
  * @Date: 8/23/2022-08-23-7:44 PM
  * @Description：PACKAGE_NAME
  */
-public class fib2 {
-}
+//public class fib2 {
+//    public static int fib2(int n, int k, int f0, int f1){
+//
+//        return fib2()
+//    }
+//
+//    public static void main(String[] args) {
+//        fib2()
+//    }
+//}
--- src/fibR.java
+++ src/fibR.java
@@ -1,9 +1,21 @@
-package PACKAGE_NAME;
-
 /**
  * @Auther: ZHU(lc))
  * @Date: 8/25/2022-08-25-4:38 PM
  * @Description：PACKAGE_NAME
  */
 public class fibR {
+    public static void main(String[] args) {
+        System.out.println(fib(9));
+    }
+
+    public static int fib(int n) {
+        if(n <= 1){
+
+            return n;
+        }
+        else{
+//test
+            return fib(n-1) + fib(n-2);
+        }
+    }
 }
