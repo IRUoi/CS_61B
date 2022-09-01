e1d69722460930de276b7d4e84d3aed548ef3cfd
IRUoi <pretvorniki@gmail.com>
2022-09-01 11:05:59 +0800

    test

---
+++ src/fib2.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+/**
+ * @Auther: ZHU(lc))
+ * @Date: 8/23/2022-08-23-7:44 PM
+ * @Description：PACKAGE_NAME
+ */
+public class fib2 {
+}
---
+++ src/fibR.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+/**
+ * @Auther: ZHU(lc))
+ * @Date: 8/25/2022-08-25-4:38 PM
+ * @Description：PACKAGE_NAME
+ */
+public class fibR {
+}
